88a404a08b50e8c931814539042237a6c78097f4
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-03-03 08:17:15 +0100

    code and repository cleaning

--- auxiliar_methods.py
+++ auxiliar_methods.py
@@ -18,9 +18,8 @@ def read_graph(graph):
             graph.add_edge(reaction, product)
             graph.add_node(product, node_type='component')
     
-    deleted = deleteHighDegreeComponents(graph)
-    if not deleted:
-        splitHighDegreeComponents(graph)
+    
+    splitHighDegreeComponents(graph)
     
     H = graph.to_undirected()
     print(f"Number of connected components: {nx.number_connected_components(H)}")
@@ -62,104 +61,6 @@ def duplicateNode(graph, node):
 
     graph.remove_node(node) 
 
-def deleteHighDegreeComponents(graph):
-    doSplit = input("Do you want to delete the high degree components? (Y/n)")
-
-    if doSplit == "Y" or doSplit == "y":
-
-        ordered_nodes = nodes_ordered_by_degree(graph)
-        print(f"Initial number of nodes: {graph.number_of_nodes()}")
-        for node, degree in ordered_nodes:
-            if degree >= 6 and graph.nodes[node]['node_type'] == 'component':
-
-                graph.remove_node(node)  
-        
-        print(f"Final number of nodes: {graph.number_of_nodes()}")  
-        return True
-    return False
-
-def edges_cross_1D(r1,r2,c1,c2):
-    return (r1 < r2 and c1 > c2) or (r2 < r1 and c2 > c1)
-
-def edges_cross_1D_2(r1,r2,c1,c2, c_y1, c_y2, r_y1, r_y2):
-    return r_y1 == r_y2 and c_y1 == c_y2 and ((r1 < r2 and c1 > c2) or (r2 < r1 and c2 > c1))
-
-def are_not_coincident(r1, r2, c1, c2):
-    return (r1 != r2) and (r1 != c1) and (r1 != c2) and (r2 != c1) and (r2 != c2) and (c1 != c2)
-
-def edges_cross_2D(r1,r2,c1,c2):
-
-    d_e1_e2_1 = determinant(r1[0],r1[1],c1[0],c1[1],r2[0],r2[1])
-    d_e1_e2_2 = determinant(r1[0],r1[1],c1[0],c1[1],c2[0],c2[1])
-
-    d_e2_e1_1 = determinant(r2[0],r2[1],c2[0],c2[1],r1[0],r1[1])
-    d_e2_e1_2 = determinant(r2[0],r2[1],c2[0],c2[1],c1[0],c1[1])
-
-
-    return d_e1_e2_1*d_e1_e2_2 < 0 and d_e2_e1_1*d_e2_e1_2 < 0
-
-def determinant(px,py,qx,qy,rx,ry):
-	return ((qx-px)*(ry-py) - (rx-px)*(qy-py))
-
-def count_edge_crossings(graph, positions, reactions):
-    crosses = 0
-    num_crosses_edge = {}
-    edge_crossing_edges = {}
-    for edge1 in graph.edges():
-        num_crosses_edge[edge1] = 0
-        edge_crossing_edges[edge1] = set()
-        for edge2 in graph.edges():
-            if edge1 != edge2:
-
-                node1 = edge1[0]
-                node2 = edge1[1]
-                node3 = edge2[0]
-                node4 = edge2[1]
-
-                r1_pos = 0
-                c1_pos = 0
-                r2_pos = 0
-                c2_pos = 0
-
-                if node1 in reactions:
-                    r1_pos = positions[node1]
-                    c1_pos = positions[node2]
-                else:
-                    r1_pos = positions[node2]
-                    c1_pos = positions[node1]
-                
-                if node3 in reactions:
-                    r2_pos = positions[node3]
-                    c2_pos = positions[node4]
-                else:
-                    r2_pos = positions[node4]
-                    c2_pos = positions[node3]
-                
-                if edges_cross_1D_2(r1_pos[0],r2_pos[0],c1_pos[0],c2_pos[0], c1_pos[1],c2_pos[1], r1_pos[1], r2_pos[1]):
-                # if edges_cross_1D(r1_pos,r2_pos,c1_pos,c2_pos) and are_not_coincident(r1_pos, r2_pos, c1_pos, c2_pos): 
-                #     print(f"Nodes: {node1} {node2} {node3} {node4}")
-                    crosses += 1
-                    num_crosses_edge[edge1] += 1
-                    edge_crossing_edges[edge1].add(edge2)
-                    
-    return crosses // 2, dict(sorted(edge_crossing_edges.items(), key=lambda item: len(item[1]), reverse=True))
-
-def changeSourceAndSinkNodeType(graph):
-    for node in graph.nodes():
-        if graph.in_degree(node) == 0:
-            graph.nodes[node]['node_type'] = 'source'
-        elif graph.out_degree(node) == 0:
-            graph.nodes[node]['node_type'] = 'sink'
-
-def get_color(node_type):
-    if node_type == 'reaction':
-        return 'blue'   
-    if node_type == 'source':
-        return 'orange'
-    if node_type == 'sink':
-        return 'green'
-    return 'red'
-    
 def nodes_ordered_by_degree(graph, nodes=None):
     """
     Returns a list of nodes ordered by degree in descending order.
@@ -185,109 +86,6 @@ def nodes_ordered_by_degree(graph, nodes=None):
 
     #return nodes_sorted_by_degree
     return degree_list
-
-def get_source_and_sink_nodes(graph):
-    nodes = graph.nodes()
-    in_degree_list = [(node, degree) for node, degree in graph.in_degree(nodes)]
-    out_degree_list = [(node, degree) for node, degree in graph.out_degree(nodes)]
-    sink_nodes, source_nodes = [], []
-    for in_deg, out_deg in  zip(in_degree_list, out_degree_list):
-        if in_deg[0] == out_deg[0]:
-            if in_deg[1] == 0: #source
-                source_nodes.append(in_deg[0])
-            if out_deg[1] == 0: #source
-                sink_nodes.append(in_deg[0])
-
-
-    return source_nodes, sink_nodes
-
-def split_components_1(graph, pos, components):
-    new_pos = pos.copy()
-
-    for node in graph.nodes():
-        if graph.degree(node) <= 3 and node in components:
-            new_pos[node] = (pos[node][0], 3)
-
-    return new_pos
-
-def flatten_list(nested_list):
-    flat_list = []
-    for item in nested_list:
-        if isinstance(item, list):
-            flat_list.extend(flatten_list(item))
-        else:
-            flat_list.append(item)
-    return flat_list
-
-def split_reactions_1(graph, pos, reactions):    
-    new_pos = pos.copy()
-
-    for node in graph.nodes():
-        if node in reactions:
-            # adjacent_nodes_iterator = graph.neighbors(node)
-            # adjacent_nodes = list(adjacent_nodes_iterator)
-            in_nodes = flatten_list(list(graph.predecessors(node)))
-            out_nodes = flatten_list(list(graph.successors(node)))
-
-            adjacent_nodes = in_nodes + out_nodes
-
-            first_node = adjacent_nodes[0]
-
-            all_in_one_side = True
-
-            for adjacent_node in adjacent_nodes:
-                if not pos[first_node][1] == pos[adjacent_node][1]:
-                    all_in_one_side = False
-                    break
-
-            if all_in_one_side:
-                if pos[node][1] < pos[first_node][1]:
-                    new_pos[node] = (pos[node][0], pos[first_node][1] + 2)
-                else:
-                    new_pos[node] = (pos[node][0], pos[first_node][1] - 2)
-
-    return new_pos
-
-def add_element_to_set(positions, key, element):
-    if key not in positions:
-        positions[key] = set()
-    positions[key].add(element)
-
-def computeStep(size_small, size_big):
-    return size_big/size_small
-
-def rearange_positions(positions):
-    new_positions = positions.copy()
-    nodes_per_y = {}
-
-    for pos in positions:
-        add_element_to_set(nodes_per_y,positions[pos][1],pos)
-    
-    max_size = max(nodes_per_y, key=lambda key: len(nodes_per_y[key]))
-    
-    for y in nodes_per_y:
-        x = 0
-        step = computeStep(len(nodes_per_y[y]), max_size)
-        for node in nodes_per_y[y]:         
-            new_positions[node] = (x,y)
-            x += step
-
-    return new_positions
-
-def splitNodesIntoDifferentLayers(G, components, reactions, pos):
-
-    doSplit = input("Do you want to split the components? (Y/n)")
-    if doSplit == "Y" or doSplit == "y":
-        pos = split_components_1(G,pos,components)
-        n_crossings, _ = count_edge_crossings(G, pos, reactions)
-
-        print(f"Number of crossings split components: {n_crossings}")
-
-        pos = split_reactions_1(G,pos,reactions)
-        n_crossings, _ = count_edge_crossings(G, pos, reactions)
-
-        print(f"Number of crossings split reactions: {n_crossings}")
-    return pos
 
 def removeCyclesByNodeInMostCycles(graph):
 
@@ -353,79 +151,31 @@ def removeCyclesByNodeInMostCycles(graph):
         H = graph.to_undirected()
         print(f"Number of connected components: {nx.number_connected_components(H)}")
 
-def removeCyclesBySourceOrSink(graph):
-    cycles = nx.recursive_simple_cycles(graph)
+def get_color(node_type):
+    if node_type == 'reaction':
+        return 'blue'   
+    if node_type == 'source':
+        return 'orange'
+    if node_type == 'sink':
+        return 'green'
+    return 'red'
+ 
+def setColorNodeType(graph):
 
-    appearances = {}
-    for cycle in cycles:
-        for node in cycle:
-            if node[0] == 'C':
-                if node in appearances:
-                    appearances[node] += 1
-                else:
-                    appearances[node] = 1
+    node_colors = {node: get_color(data['node_type']) for node, data in graph.nodes(data=True)}
+    nx.set_node_attributes(graph, node_colors, 'color')
 
-    sortedAppearances = sorted(appearances.items(), key=lambda x:x[1],reverse=True)
+    colors = [data['color'] for _, data in graph.nodes(data=True)]
 
-    for app in sortedAppearances:
-        node = app[0]
-        if graph.in_degree(node) == 0 or graph.out_degree(node) == 0:
-            duplicateNode = input(f"Do you want to duplicate {node}? (Y/n)")
-            if duplicateNode == "y" or duplicateNode == "Y":
-                print(f"Initial number of nodes: {graph.number_of_nodes()}")
-                
-                out_edges = graph.out_edges([node])
-                in_edges = graph.in_edges([node])
+    return colors
 
-                i = 0
-                for out_edge in out_edges:
-                    new_node = "D" + str(i) + '_' + node
-                    i += 1
-                    graph.add_edge(new_node, out_edge[1])
-                    graph.add_node(new_node, node_type='component')
-                
-                for in_edge in in_edges:
-                    new_node = "D" + str(i) + '_'  + node
-                    i += 1
-                    graph.add_edge(in_edge[0], new_node)
-                    graph.add_node(new_node, node_type='component')
+def changeSourceAndSinkNodeType(graph):
+    for node in graph.nodes():
+        if graph.in_degree(node) == 0:
+            graph.nodes[node]['node_type'] = 'source'
+        elif graph.out_degree(node) == 0:
+            graph.nodes[node]['node_type'] = 'sink'
 
-                graph.remove_node(node) 
-            else:
-                return
-
-            print(f"Final number of nodes: {graph.number_of_nodes()}")  
-            cycles = nx.recursive_simple_cycles(graph)
-            print(f"Number of cycles: {len(cycles)}")
-            if len(cycles) == 0 : return
-
-            appearances = {}
-            for cycle in cycles:
-                for node in cycle:
-                    if node[0] == 'C':
-                        if node in appearances:
-                            appearances[node] += 1
-                        else:
-                            appearances[node] = 1
-
-            sortedAppearances = sorted(appearances.items(), key=lambda x:x[1],reverse=True)
-            
-            H = graph.to_undirected()
-            print(f"Number of connected components: {nx.number_connected_components(H)}")
-
-def analyzeCycles(graph):
-
-    cycles = nx.recursive_simple_cycles(graph)
-    print(f"Number of cycles: {len(cycles)}")
-    
-    removeCyclesBySourceOrSink(graph)
-
-def numberLevels(pos):
-    levels = set()
-    for node in pos:
-        levels.add(pos[node])
-    return len(levels)
-        
 def rearangeSources(graph, pos):
     for node in pos:
         if graph.in_degree(node) == 0:
@@ -439,7 +189,7 @@ def rearangeSources(graph, pos):
             else:
                 newPosX = posNeigh[0]+40
             
-            # si lunic veí de neighbor més petit que té és node, posar node a mateixa x
+            # if the only neighbor lower of neighbor is node, put node at same x as neighbor
             belowNeighbors = 0
             for neigh in graph.predecessors(neighbor):
                 print(neigh, pos[neigh])
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import sys
 
 import auxiliar_methods as funcs
 import layouts as lyts
-import hill_climbing as hc
-
 
 import grandalf
 from grandalf.layouts import SugiyamaLayout
@@ -16,64 +14,18 @@ import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
 reactions = []
-# colors = []
 
 def main():
 
     reactions, components = funcs.read_graph(G)
 
     funcs.removeCyclesByNodeInMostCycles(G)
-    funcs.splitHighDegreeComponents(G)
     funcs.changeSourceAndSinkNodeType(G)
 
-    node_colors = {node: funcs.get_color(data['node_type']) for node, data in G.nodes(data=True)}
-    nx.set_node_attributes(G, node_colors, 'color')
-
-    colors = [data['color'] for _, data in G.nodes(data=True)]
-
-
-    mode = input("Select mode:\n     1: Lau\n     2: Sugiyama\n     3: DAG\n     4: CCs\n")
-    match mode:
-        case "1": 
-            lau(colors)
-        case "2":
-            sugiyama(colors)
-        case "3":
-            dag(colors)
-        case "4":
-            connectedComponents(colors)
-
-def lau(colors):        
-    pos = lyts.selectInitialLayout(G)
-
-    if pos == None:
-        print("Please, select valid layout")
-        return
-
-    n_crossings, _ = funcs.count_edge_crossings(G, pos, reactions)
-    print(f"Initial number of crossings: {n_crossings}")
-
-    # subax1 = plt.subplot(131)    
-    # nx.draw(G, pos, node_color=colors)
-
-    # pos = funcs.splitNodesIntoDifferentLayers(G, components, reactions, pos)
-    n_crossings, _ = funcs.count_edge_crossings(G, pos, reactions)
-
-    print(f"Final number of crossings: {n_crossings}")
-
-    source, sink = funcs.get_source_and_sink_nodes(G)
-
-    print(f"Number of source nodes: {len(source)}")
-    # print(source)
-    print(f"Number of sink nodes: {len(sink)}")
-    # print(sink)
-    # subax3 = plt.subplot(133)
-
-    pos = funcs.rearange_positions(pos)
-
-    nx.draw(G, pos, node_color=colors)
-    # nx.draw(G, pos=nx.spring_layout(G))  
-    plt.show()
+    colors = funcs.setColorNodeType(G)
+    
+    sugiyama(colors)
+   
 
 def sugiyama(colors):
 
@@ -98,30 +50,6 @@ def sugiyama(colors):
     nx.draw(G, pos=poses, with_labels=True, node_color=colors)
     
     plt.show()
-
-def dag(colors):
-    for layer, nodes in enumerate(nx.topological_generations(G)):
-        # `multipartite_layout` expects the layer as a node attribute, so add the
-        # numeric layer value as a node attribute
-        for node in nodes:
-            G.nodes[node]["layer"] = layer
-
-    # Compute the multipartite_layout using the "layer" node attribute
-    pos = nx.multipartite_layout(G, subset_key="layer")
-
-    fig, ax = plt.subplots()
-    nx.draw_networkx(G, pos=pos, ax=ax, node_color=colors)
-    ax.set_title("DAG layout in topological order")
-    fig.tight_layout()
-    plt.show()
-
-
-def connectedComponents(colors):
-    H = G.to_undirected()
-    S = [H.subgraph(c).copy() for c in nx.connected_components(H)]
-    for subGraph in S:
-        nx.draw(subGraph, with_labels=True)
-        plt.show()
     
 if __name__ == "__main__":
     main()
