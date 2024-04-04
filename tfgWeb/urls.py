c52b54cc99222052d86915f5481fbc06b104952d
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-04-04 09:18:38 +0200

    updating folder structure (II)

--- tfgWeb/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import index
-
-urlpatterns = [
-    path("tfgWeb/", include("tfgWeb.urls")),
-    path("admin/", admin.site.urls),
-]
